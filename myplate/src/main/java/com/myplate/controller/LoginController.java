b50c334ee5a2b91c763747063faa373350ab8e80
dujianjun <1208jun1013>
2018-10-10 10:06:15 +0800

    测试分支

--- myplate/src/main/java/com/myplate/controller/LoginController.java
+++ myplate/src/main/java/com/myplate/controller/LoginController.java
@@ -6,11 +6,9 @@ import javax.websocket.server.PathParam;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
